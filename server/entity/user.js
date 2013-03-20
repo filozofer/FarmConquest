23e97d36638b701e7ee59551ef53a6ac15a0a814
Arnaud Bretagne <arnaud.bretagne@hotmail.fr>
2013-03-20 17:24:06 +0100

    User class

---
+++ server/entity/user.js
@@ -0,0 +1,9 @@
+function User(username, mail, password, dateOfCreation) {
+  this.username = username;
+  this.mail = mail;
+  this.password = password;
+  this.dateOfCreation = dateOfCreation;
+
+  //Methods....
+  //this.checkSomething = checkSomething();
+}
