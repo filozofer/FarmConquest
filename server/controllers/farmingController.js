4120c22124ff8ab56a5fb6aa0814a19d56f0f30d
Baron Lucien <lucien.baron@hotmail.fr>
2013-06-12 22:31:32 +0200

    Gestion de l'arrosage et de la fertilisation

--- server/controllers/farmingController.js
+++ server/controllers/farmingController.js
@@ -9,8 +9,23 @@ FarmingController = function(socket, db, mongoose){
 
     var farmingActions = ["arrosage","fertilisation"];
 
-    socket.on('doFarmingAction', function(selectedFarmingAction){
-        console.log(farmingActions[selectedFarmingAction]);
+    socket.on('doFarmingAction', function(tile, index){
+        var action = farmingActions[index];
+
+        switch(action){
+            case "arrosage" :
+                if(tile.humidity < 10) {
+                    tile.humidity++;
+                }
+                break;
+            case "fertilisation" :
+                if(tile.fertility < 10) {
+                    tile.fertility++;
+                }
+                break;
+            default:
+                break;
+        }
     });
 
 };
