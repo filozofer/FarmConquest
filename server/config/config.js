ea08487822489a54d49eae0be9cffec7e1da452f
Baron Lucien <lucien.baron@hotmail.fr>
2013-06-12 22:29:25 +0200

    Ajout de plusieurs configs de base

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,15 @@
 Configuration = function(){
 
     this.farmSize = 20;
+    // rayon de communication pour l'affichage des messages du chat
     this.communicationRadius = 50;
+    // price of water action
+    this.waterPrice = 5;
+    // price of fertilizer action
+    this.fertilizerPrice = 10;
+    // initial amount of money the player is given
+    this.initialMoney = 500;
+
 
     //Tile content types
 
