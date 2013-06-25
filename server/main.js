958003e58b86d7513bcdd06930ab4d4f4c4d2846
97207 <97207@supinfo.com>
2013-06-25 16:25:24 +0200

    beta 1.26 ================= - Params for server =================

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ var mongoose    = require('mongoose');
 var db          = require('./lib/db');
 
 // OPENSHIFT //
-//var express		= require('express');
+var express		= require('express');
 //var connect		= require('connect');
 //var fs			= require('fs');
 // END OPENSHIFT //
