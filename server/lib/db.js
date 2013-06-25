4fcd49bf5a6a52aefd7f061b695b55c8d4ff5905
97207 <97207@supinfo.com>
2013-06-25 18:08:13 +0200

    beta 1.27 ================= - Server/Local configurations =================

--- server/lib/db.js
+++ server/lib/db.js
@@ -9,7 +9,7 @@
 var mongoose = require('mongoose');
 
 module.exports = function () {
-    //var database = mongoose.connect('mongodb://127.0.0.1/FarmConquest', //KIKOO
+    //var database = mongoose.connect('mongodb://127.0.0.1/FarmConquest', //LOCAL ADDRESS
     var database = mongoose.connect('mongodb://admin:_GbXzbqKNDqS@5149baae5973caaf18000239-conquest.rhcloud.com:59616/farm',
         function(err) {
             if (err) { throw err; }
